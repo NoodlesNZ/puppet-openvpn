55eb49e207055350099f4111b4371790f5c169b1
Niklaus Hofer <niklaus.hofer@stepping-stone.ch>
2018-09-26 16:37:48 +0200

    openvpn::revoke: Update command to work with easyrsa 3

--- manifests/revoke.pp
+++ manifests/revoke.pp
@@ -60,7 +60,7 @@ define openvpn::revoke (
   $etc_directory = $::openvpn::params::etc_directory
 
   exec { "revoke certificate for ${name} in context of ${server}":
-    command  => ". ./vars && ./revoke-full ${name}; echo \"exit $?\" | grep -qE '(error 23|exit (0|2))' && touch revoked/${name}",
+    command  => ". ./vars && ./easyrsa --batch revoke ${name}; echo \"exit $?\" | grep -qE '(error 23|exit (0|2))' && touch revoked/${name}",
     cwd      => "${etc_directory}/openvpn/${server}/easy-rsa",
     creates  => "${etc_directory}/openvpn/${server}/easy-rsa/revoked/${name}",
     provider => 'shell',
